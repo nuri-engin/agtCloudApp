bb789215c07c4fb8bb1bfc4c3b2383b1f0b86a68
nuri engin <uzakkultur@gmail.com>
2019-10-15 22:35:57 +0300

    Updates

---
+++ precache-manifest.f383b477bb0d7f83802d52957bfe38d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bcce640e84f08db004172f755e569d",
+    "url": "/agtCloudApp/index.html"
+  },
+  {
+    "revision": "4a7a40dd4ad9f9f08d10",
+    "url": "/agtCloudApp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dc5904a3957ad1b61678",
+    "url": "/agtCloudApp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4a7a40dd4ad9f9f08d10",
+    "url": "/agtCloudApp/static/js/2.c202aafe.chunk.js"
+  },
+  {
+    "revision": "dc5904a3957ad1b61678",
+    "url": "/agtCloudApp/static/js/main.b5b29c41.chunk.js"
+  },
+  {
+    "revision": "9b083d5b583707cc64a4",
+    "url": "/agtCloudApp/static/js/runtime-main.e5e05aa3.js"
+  }
+]);
